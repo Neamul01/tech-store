52e79cd8700a056091874ccf6f4d151acb504973
Neamul01 <neamulhaque2002@gmail.com>
2022-04-04 13:03:03 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/reviews' element={<Review></Review>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar component</h2>
-        </div>
+        <nav className='mb-4 p-8 shadow bg-gray-100 text-right navbar-custom' >
+            <Link to={'/home'}>Home</Link>
+            <Link to={'/reviews'}>Reviews</Link>
+            <Link to={'/dashboard'}>Dashboard</Link>
+            <Link to={'/blogs'}>Blogs</Link>
+            <Link to={'about'}>About</Link>
+        </nav>
     );
 };
 
