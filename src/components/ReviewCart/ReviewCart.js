e047f9009d8e82387c544f0d3e504b423cc1bc1e
Neamul01 <neamulhaque2002@gmail.com>
2022-04-04 16:22:06 +0600

    review star added

--- src/components/ReviewCart/ReviewCart.js
+++ src/components/ReviewCart/ReviewCart.js
@@ -1,6 +1,5 @@
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import ReactStars from 'react-rating-stars-component';
 import './ReviewCart.css'
 
 const ReviewCart = ({ review }) => {
@@ -13,9 +12,13 @@ const ReviewCart = ({ review }) => {
             <div className='text-left ml-4'>
                 <h3 className="text-3xl text-orange-400 mb-4">{name}</h3>
                 <p className=''>{description}</p>
-                <p className='text-orange-300 font-bold mt-4'><FontAwesomeIcon className='mx-2' icon={faStar}></FontAwesomeIcon>{rating}</p>
+                <p className='text-orange-300 font-bold mt-4'><ReactStars
+                    count={rating}
+                    size={24}
+                    color="#ffd700"
+                /></p>
             </div>
-        </div>
+        </div >
     );
 };
 
