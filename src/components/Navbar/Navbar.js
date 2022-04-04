848567c5c78c026de3b861373c72cfb97623f882
Neamul01 <neamulhaque2002@gmail.com>
2022-04-04 13:38:03 +0600

    navbar logo and spacing dome

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import img from '../../logo.png'
 
 const Navbar = () => {
     return (
-        <nav className='mb-4 p-8 shadow bg-gray-100 text-right navbar-custom' >
-            <Link to={'/home'}>Home</Link>
-            <Link to={'/reviews'}>Reviews</Link>
-            <Link to={'/dashboard'}>Dashboard</Link>
-            <Link to={'/blogs'}>Blogs</Link>
-            <Link to={'about'}>About</Link>
+        <nav className='flex items-center justify-between shadow bg-gray-100 text-right p-2' >
+            <Link to={'/'}><img className='w-24 m-0 p-0 ml-4' src={img} alt="" /></Link>
+            <div className=' navbar-custom'>
+                <Link to={'/home'}>Home</Link>
+                <Link to={'/reviews'}>Reviews</Link>
+                <Link to={'/dashboard'}>Dashboard</Link>
+                <Link to={'/blogs'}>Blogs</Link>
+                <Link to={'about'}>About</Link>
+            </div>
         </nav>
     );
 };
