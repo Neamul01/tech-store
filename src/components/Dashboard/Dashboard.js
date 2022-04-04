54ccda6047777b675382e1fb54b448271cf695a3
Neamul01 <neamulhaque2002@gmail.com>
2022-04-04 20:48:15 +0600

    chart done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [data, setData] = useState([])
@@ -7,8 +7,9 @@ const Dashboard = () => {
         fetch('data.json').then(res => res.json()).then(data => setData(data))
     }, [])
     return (
-        <div>
-            <ResponsiveContainer width="100%" height="100%">
+        <div className='mt-8 flex justify-around' >
+            <div>
+                <h2 className="text-4xl text-orange-400 mb-16">Revenue Chart</h2>
                 <LineChart
                     width={500}
                     height={300}
@@ -20,15 +21,22 @@ const Dashboard = () => {
                         bottom: 5,
                     }}
                 >
-                    <CartesianGrid />
+                    <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="month" />
                     <YAxis dataKey={'sell'} />
                     <Tooltip />
                     <Legend />
-                    <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
                     <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
                 </LineChart>
-            </ResponsiveContainer>
+            </div>
+            <div>
+                <h2 className="text-4xl text-orange-400">Customer Chart</h2>
+                <PieChart width={400} height={400}>
+                    <Pie data={data} dataKey="sell" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" label />
+                    <Pie data={data} dataKey="month" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" />
+                </PieChart>
+            </div>
         </div>
     );
 };
