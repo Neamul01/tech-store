d7782d11eaf5ee4fac7e9be26ef56e027fa9a882
Neamul01 <neamulhaque2002@gmail.com>
2022-04-05 13:39:49 +0600

    custom link added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.css'
 import img from '../../logo.png'
+import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
         <nav className='flex items-center justify-between shadow bg-gray-100 text-right p-2' >
-            <Link to='/'><img className='w-24 m-0 p-0 ml-4' src={img} alt="" /></Link>
-            <div className=' navbar-custom'>
-                <Link to='/home'>Home</Link>
-                <Link to='/reviews'>Reviews</Link>
-                <Link to='/dashboard'>Dashboard</Link>
-                <Link to='/blogs'>Blogs</Link>
-                <Link to='about'>About</Link>
+            <CustomLink to='/'><img className='w-24 m-0 p-0 ml-4' src={img} alt="" /></CustomLink>
+            <div className=' flex'>
+                <CustomLink className=' navbar-custom' to='/home'>Home</CustomLink>
+                <CustomLink className=' navbar-custom' to='/reviews'>Reviews</CustomLink>
+                <CustomLink className=' navbar-custom' to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink className=' navbar-custom' to='/blogs'>Blogs</CustomLink>
+                <CustomLink className=' navbar-custom' to='about'>About</CustomLink>
             </div>
         </nav>
     );
