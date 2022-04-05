3096b459f3f271db7b686d1824fc1a3e6630c8ae
Neamul01 <neamulhaque2002@gmail.com>
2022-04-05 13:13:45 +0600

    one more chart added and exists chart modified

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [data, setData] = useState([])
@@ -7,7 +7,7 @@ const Dashboard = () => {
         fetch('data.json').then(res => res.json()).then(data => setData(data))
     }, [])
     return (
-        <div className='mt-8 flex justify-around ' >
+        <div className='mt-8 grid grid-cols-2 justify-items-center' >
             <div>
                 <h2 className="text-4xl text-orange-400 mb-16">Revenue Chart</h2>
                 <LineChart
@@ -33,9 +33,18 @@ const Dashboard = () => {
             <div>
                 <h2 className="text-4xl text-orange-400">Customer Chart</h2>
                 <PieChart width={400} height={400}>
-                    <Pie data={data} dataKey="sell" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" label />
-                    <Pie data={data} dataKey="month" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" />
+                    <Pie data={data} dataKey="sell" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label />
                 </PieChart>
+            </div>
+            <div>
+                <h2 className="text-4xl text-orange-400 mb-8">More Data</h2>
+                <BarChart width={500} height={400} data={data}>
+                    <Bar dataKey="sell" fill="#8884d8" />
+                    <Tooltip />
+                    <Legend />
+                    <XAxis dataKey={'month'} />
+                    <YAxis dataKey={'sell'} />
+                </BarChart>
             </div>
         </div>
     );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,13 +6,13 @@ import img from '../../logo.png'
 const Navbar = () => {
     return (
         <nav className='flex items-center justify-between shadow bg-gray-100 text-right p-2' >
-            <Link to={'/'}><img className='w-24 m-0 p-0 ml-4' src={img} alt="" /></Link>
+            <Link to='/'><img className='w-24 m-0 p-0 ml-4' src={img} alt="" /></Link>
             <div className=' navbar-custom'>
-                <Link to={'/home'}>Home</Link>
-                <Link to={'/reviews'}>Reviews</Link>
-                <Link to={'/dashboard'}>Dashboard</Link>
-                <Link to={'/blogs'}>Blogs</Link>
-                <Link to={'about'}>About</Link>
+                <Link to='/home'>Home</Link>
+                <Link to='/reviews'>Reviews</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='about'>About</Link>
             </div>
         </nav>
     );
